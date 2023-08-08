b28af5e3e316850770a2aa0ff03cdaff27b66965
SUNGIL CHO <67699933+sung3441@users.noreply.github.com>
2023-08-08 23:15:21 +0900

    add: stream api

---
+++ src/main/java/study/chapter9/README.md
@@ -0,0 +1,4 @@
+# Stream API
+
+- Collection interface를 상속 받는 모든 클래스는 stream을 사용할 수 있다.
+
---
+++ src/main/java/study/chapter9/stream/FilterMapMain.java
@@ -0,0 +1,24 @@
+package study.chapter9.stream;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FilterMapMain {
+
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        // stream 적용 전
+        List<Integer> forList = new ArrayList<>();
+
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) % 2 == 0) {
+                forList.add(list.get(i) * 10);
+            }
+        }
+
+        // stream 적용 후
+        List<Integer> filterMapList = list.stream().filter(i -> i % 2 == 0).map(i -> i * 10).toList();
+    }
+}
